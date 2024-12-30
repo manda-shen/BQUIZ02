24c31420ac8ee80cf15dad97b46b37e318fe9bb0
Manda <mandashen@gmail.com>
2024-12-30 15:08:06 +0800

    完成忘記密碼註冊頁面

---
+++ front/reg.php
@@ -0,0 +1,32 @@
+<fieldset style="width:50%;margin:auto;">
+    <legend>會員註冊</legend>
+    <div style="color:red">
+        *請設定您要註冊的帳號及密碼(最常12個字元)
+    </div>
+    <table>
+        <tr>
+            <td>Stpe1:登入帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>Step2:密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>Step3:再次確認密碼</td>
+            <td><input type="password" name="pw2" id="pw2"></td>
+        </tr>
+        <tr>
+            <td>Stpe4:信箱</td>
+            <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" value="註冊">
+                <input type="reset" value="清除">
+            </td>
+            <td>
+            </td>
+        </tr>
+    </table>
+</fieldset>
