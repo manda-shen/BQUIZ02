d865ba8cf781cf588e47affb388ab62b20266afb
Manda <mandashen@gmail.com>
2025-01-03 09:59:03 +0800

    更新問卷資料表、完成分類網誌取得文章功能

--- back/que.php
+++ back/que.php
@@ -1,17 +1,16 @@
-<fieldset style="width:75%;">
+<fieldset style="width:70%;margin:auto">
     <legend>新增問卷</legend>
-    <table style="width:95%;">
-        <tr style>
-            <td class="clo" style="width:12%;">問卷名稱</td>
+    <table style="width:100%">
+        <tr>
+            <td class='clo'>問卷名稱</td>
             <td>
-                <input type="text" name="subject" id="subject" style="width:85%;">
+                <input type="text" name="subject" id="subject" style="width:80%">
             </td>
         </tr>
-
-        <tr>
-            <td class="clo" colspan='2' style="width:95%;">
+        <tr class='clo'>
+            <td colspan='2'>
                 <div id="options">
-                    選項&nbsp;<input type="text" name="option[]" id="" style="width:80%;">
+                    選項<input type="text" name="option[]" id="" style="width:80%">
                     <button onclick="more()">更多</button>
                 </div>
             </td>
@@ -25,21 +24,21 @@
 
 <script>
 function more() {
-    let el = `<div id="options">選項&nbsp;<input type="text" name="option[]" id="" style="width:80%;"></div>`
+    let el = `<div>選項<input type="text" name="option[]" id="" style="width:80%"></div>`
     $("#options").before(el)
 }
 
 function send() {
     let subject = $("#subject").val()
     let options = $("input[name='option[]']").map((id, item) => $(item).val()).get()
-    console.log(subject, options)
+    //console.log(subject,options)
 
     $.post("./api/add_que.php", {
         subject,
         options
     }, (res) => {
-        console.log(res)
-        //location.reload()
+        //console.log(res)
+        location.reload()
     })
 }
 
--- front/po.php
+++ front/po.php
@@ -1,1 +1,55 @@
-分類網誌
+<style>
+.type,
+.list-item {
+    display: block;
+    margin: 10px 0;
+}
+</style>
+
+<div>
+    目前位置：首頁 > 分類網誌 > <span id='type'>健康新知</span>
+</div>
+<fieldset style="width:150px;margin-top:10px;display:inline-block;vertical-align:top">
+    <legend>分類網誌</legend>
+    <a href="#" class='type' data-type='1'>健康新知</a>
+    <a href="#" class='type' data-type='2'>菸害防治</a>
+    <a href="#" class='type' data-type='3'>癌症防治</a>
+    <a href="#" class='type' data-type='4'>慢性病防治</a>
+</fieldset>
+
+<fieldset style='width:500px;display:inline-block;margin-top:10px;'>
+    <legend>文章列表</legend>
+    <div id="postList"></div>
+</fieldset>
+
+<script>
+// function getList(){
+//     $('.type')
+// }
+$(".type").on('click', function() {
+    // console.log($(this).text())
+    // console.log(e)
+    $("#type").text($(this).text())
+    let type = $(this).data('type')
+    getList(type)
+
+})
+
+function getList(type) {
+    /**
+     * 1.有參數時，等同使用$.post
+     * 2.無參數時，等同使用$.get
+     */
+    $("#postList").load("./api/get_list.php", {
+        type
+    })
+    /*
+    $.get("./api/get_list.php",{type},(list)=>{
+    $("#postList").html(list) 
+     */
+}
+
+function getPost(id) {
+    $("#postList").load("./api/get_post.php", {id})
+}
+</script>
